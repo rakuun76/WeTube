58788b9ebf8e43b2bd528e7dee6c6ffc3457bfcb
rakuun76 <rakuun76@gmail.com>
2023-10-21 21:54:08 +0900

    morgan logger middleware 추가

--- src/server.js
+++ src/server.js
@@ -1,11 +1,16 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
+
 const app = express();
+const logger = morgan("dev");
 
 const handleHome = (req, res) => {
-  return res.send("Hello Fxcking world!!");
+  return res.send("<h1>Home Sweet Home</h1>");
 };
+
+app.use(logger);
 app.get("/", handleHome);
 
 app.listen(PORT);

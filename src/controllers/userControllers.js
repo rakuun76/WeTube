82fdbe98ac0787530f68dedcf253e5898186e989
rakuun76 <rakuun76@gmail.com>
2023-11-14 16:44:50 +0900

    Login 구현

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,4 +1,5 @@
 import User from "../models/User";
+import bcrypt from "bcrypt";
 
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
 export const postJoin = async (req, res) => {
@@ -16,7 +17,33 @@ export const postJoin = async (req, res) => {
       errorMessage: "This name/email is already taken.",
     });
   }
-  await User.create({ name, email, password, confirm });
-  return res.redirect("login");
+  try {
+    await User.create({ name, email, password, confirm });
+    return res.redirect("login");
+  } catch (error) {
+    return res
+      .status(400)
+      .render("join", { pageTitle: "Join", errorMessage: error._message });
+  }
 };
-export const getLogin = (req, res) => res.send("login");
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Login" });
+
+export const postLogin = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).render("login", {
+      pageTitle: "Login",
+      errorMessage: "An account with this email does not exist.",
+    });
+  }
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    return res.status(400).render("login", {
+      pageTitle: "Login",
+      errorMessage: "Wrong password.",
+    });
+  }
+  return res.redirect("/");
+};
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,12 +1,17 @@
 import express from "express";
 import { home, search } from "../controllers/videoControllers";
-import { getJoin, postJoin, getLogin } from "../controllers/userControllers";
+import {
+  getJoin,
+  postJoin,
+  getLogin,
+  postLogin,
+} from "../controllers/userControllers";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", home);
 globalRouter.get("/search", search);
 globalRouter.route("/join").get(getJoin).post(postJoin);
-globalRouter.route("/login").get(getLogin);
+globalRouter.route("/login").get(getLogin).post(postLogin);
 
 export default globalRouter;
